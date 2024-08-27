f46884ade00ea6b6dae20760084bfa66074cf906
CarlosTorresSi <88949477+CarlosTorresSi@users.noreply.github.com>
2024-08-27 00:42:54 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+function encriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  //Decidi poner imagenes de https://pixabay.com/ para dar un aspecto mas unico en este proyecto//
+  let muñeco = document.getElementById("muñeco");
+
+  let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+
+  if (texto.length != 0) {
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto encriptado con éxito";
+    parrafo.textContent = "";
+    muñeco.src = "./img/encriptado.jpg";
+  } else {
+    muñeco.src = "./img/muñeco.png";
+    tituloMensaje.textContent = "Ningún mensaje fue encontrado, vuelve a intentar";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    swal("Espera debias poner tu mensaje secreto, porque no lo hiciste", "Debes ingresar un texto", "warning");
+  }
+}
+
+function desencriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muñeco = document.getElementById("muñeco");
+
+  let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+  
+    if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Texto desencriptado con éxito";
+      parrafo.textContent = "";
+      muñeco.src = "./img/desencriptado.jpg";
+    } else {
+      muñeco.src = "./img/muñeco.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      swal("Espera debias poner tu mensaje secreto, porque no lo hiciste", "Debes ingresar un texto", "warning");
+    }
+}
